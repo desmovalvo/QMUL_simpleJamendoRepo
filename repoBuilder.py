5c0e0a02edaaacfb3d9cb4bc104f72736eec6879
Fabio Viola <desmovalvo@gmail.com>
2018-03-15 18:32:49 +0100

    Converted the local repo into a jamendo repo

--- repoBuilder.py
+++ repoBuilder.py
@@ -2,88 +2,66 @@
 
 # global reqs
 from sepy.LowLevelKP import *
-import eyed3
-import os
+import configparser
+import urllib
+import json
 
 # settings
-LOCAL_PATH = "audio/"
-ABSOLUTE_PATH = os.getcwd() + LOCAL_PATH
+CONFIG_FILE = "repoSettings.conf"
+JAMENDO_TRACKS = "http://api.jamendo.com/v3.0/tracks?client_id="
 SEPA_UPDATE_URI = "http://localhost:8000/update"
-NS = "http://ns#"
 QMUL = "http://eecs.qmul.ac.uk/wot#"
 DC = "http://purl.org/dc/elements/1.1/"
 AC = "http://audiocommons.org/ns/audiocommons#"
 RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-PREFIXES = """PREFIX ns: <%s> 
-PREFIX ac: <%s>
-PREFIX dc: <%s>
-PREFIX qmul: <%s> 
-PREFIX rdf: <%s>
-""" % (NS, AC, DC, QMUL, RDF)
-
-# templates
-SONG_TITLE = " %s dc:title %s . "
 
 # main
 if __name__ == "__main__":
 
+    # statements
+    prefixes = []
+    statements = []
+
+    # initialize statements with prefixes
+    prefixes.append("PREFIX qmul: <%s> " % QMUL)
+    prefixes.append("PREFIX dc: <%s> " % DC)
+    prefixes.append("PREFIX ac: <%s> " % AC)
+    prefixes.append("PREFIX rdf: <%s> " % RDF)
+
+    # then, put insert data
+    
     # create a KP
     kp = LowLevelKP()
+
+    # read config file
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+    jamendoClientId = config["Jamendo"]["clientId"]
     
-    # cycle over mp3 files in folder
-    for f in os.listdir(LOCAL_PATH):
-        if f.endswith('.mp3'):
+    # query Jamendo API
+    r = requests.get(JAMENDO_TRACKS + jamendoClientId)
+    reply = json.loads(r.text)
 
-            # initialise a SPARQL update
-            upd = PREFIXES + "INSERT DATA { "
+    # cycle over results
+    for result in reply["results"]:
 
-            # generate an URI
-            songUriClip = NS + f + "_clip"
-            songUriFile = NS + f + "_file"
-            print("Generated URI for AudioClip: " + songUriClip)
-            print("Generated URI for AudioFile: " + songUriFile)
-                        
-            # load id3 tags
-            id3tags = eyed3.load(LOCAL_PATH + f)
-            
-            # publish every file
-            upd += " <%s> rdf:type ac:AudioClip . " % (songUriClip)
-            upd += " <%s> dc:title '%s' . " % (songUriClip, id3tags.tag.title)
-            upd += " <%s> ac:available_as <%s> . " % (songUriClip, songUriFile)
-            upd += " <%s> rdf:type ac:AudioFile . " % (songUriFile)
-            upd += " <%s> ns:hasLocation '%s' " % (songUriFile, ABSOLUTE_PATH + f)
-            upd += "}"
+        # generate triples
+        statements.append(" <%s> rdf:type ac:AudioClip . " % result["shorturl"])
+        statements.append(" <%s> dc:title '%s' . " % (result["shorturl"], urllib.parse.quote(result["name"])))
+        statements.append(" <%s> ac:available_as <%s> . " % (result["shorturl"], result["audiodownload"]))
+        statements.append(" <%s> rdf:type ac:AudioFile . " % result["audiodownload"])
 
-            # do the update
-            kp.update(SEPA_UPDATE_URI, upd)
+    # build the update to insert data
+    upd = ' '.join(prefixes) + " INSERT DATA { " + ' '.join(statements) + " }"
+    print(upd)
+    kp.update(SEPA_UPDATE_URI, upd)
 
     # wait until the end, then delete data
     input("Press <ENTER> to quit")
 
-    # cycle over mp3 files in folder
-    for f in os.listdir(LOCAL_PATH):
-        if f.endswith('.mp3'):
-
-            # initialise a SPARQL update
-            upd = PREFIXES + "DELETE DATA { "
-
-            # generate an URI
-            songUriClip = NS + f + "_clip"
-            songUriFile = NS + f + "_file"
-            print("Deleting AudioClip: " + songUriClip)
-                        
-            # load id3 tags
-            id3tags = eyed3.load(LOCAL_PATH + f)
-            
-            # publish every file
-            upd += " <%s> rdf:type ac:AudioClip . " % (songUriClip)
-            upd += " <%s> dc:title '%s' . " % (songUriClip, id3tags.tag.title)
-            upd += " <%s> ac:available_as <%s> . " % (songUriClip, songUriFile)
-            upd += " <%s> rdf:type ac:AudioFile . " % (songUriFile)
-            upd += " <%s> ns:hasLocation '%s' " % (songUriFile, ABSOLUTE_PATH + f)
-            upd += "}"
-
-            # do the update
-            kp.update(SEPA_UPDATE_URI, upd)
+    # build the update to delete data
+    upd = ' '.join(prefixes) + " DELETE DATA { " + ' '.join(statements) + " }"
+    kp.update(SEPA_UPDATE_URI, upd)
     
+    # exit
     print("Bye!")
